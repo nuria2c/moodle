f329a27199c2a62e337633d574ee92c682dc264b
Issam Taboubi <issam.taboubi@umontreal.ca>
2016-09-14 11:26:02 -0400

    MDL-55586 blocks_lp: Translation strings in competencies

--- blocks/lp/version.php
+++ blocks/lp/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016052300;
+$plugin->version   = 2016091400;
 $plugin->requires  = 2016051900;
 $plugin->component = 'block_lp';
 $plugin->dependencies = array(

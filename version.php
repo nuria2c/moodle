362ba03bb84eb14db8e6577d507e7c86603b80e2
Jean-Philippe Gaudreau <jp.gaudreau@umontreal.ca>
2016-11-14 10:18:54 -0500

    MDLUM-4942 - Faire la fusion du code StudiUM avec la version 3.1.3

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016091200;
+$plugin->version   = 2016111400;
 $plugin->requires  = 2016051900;
 $plugin->component = 'theme_cleanudem';
 $plugin->dependencies = array(

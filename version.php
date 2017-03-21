2b7b76434fe66734a2a8c78d97630624c9bcd2c5
David Ligne <david.ligne@umontreal.ca>
2017-03-21 10:07:39 -0400

    MDLUM-5166 : Faire la fusion du code StudiUM avec la version 3.2.2

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2017021600;
+$plugin->version   = 2017032100;
 $plugin->requires  = 2016051900;
 $plugin->component = 'theme_cleanudem';
 $plugin->dependencies = array(

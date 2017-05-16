e3a8fe7aa308e4e5ca50f85b6395f02c6ec55ce5
Issam Taboubi <issam.taboubi@umontreal.ca>
2017-05-16 10:46:42 -0400

    EVOSTDM-870 - Auto-éval/éval pairs - Wizard - Résumé

--- mod/workshop/classes/wizard/summary_step_form.php
+++ mod/workshop/classes/wizard/summary_step_form.php
@@ -19,6 +19,7 @@
  *
  * @package    mod_workshop
  * @author     Gilles-Philippe Leblanc <gilles-philippe.leblanc@umontreal.ca>
+ * @author     Issam Taboubi <issam.taboubi@umontreal.ca>
  * @copyright  2017 Université de Montréal
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
@@ -34,6 +35,7 @@ require_once($CFG->libdir . '/formslib.php');
  *
  * @package    mod_workshop
  * @author     Gilles-Philippe Leblanc <gilles-philippe.leblanc@umontreal.ca>
+ * @author     Issam Taboubi <issam.taboubi@umontreal.ca>
  * @copyright  2017 Université de Montréal
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
@@ -43,6 +45,90 @@ class summary_step_form extends step_form {
      * The step form definition.
      */
     public function step_definition() {
+        global $DB, $USER;
+        $mform = $this->_form;
+        $record = $this->workshop->get_record();
+        // Summary container.
+        $mform->addElement('html', \html_writer::start_div('wizard-summary'));
+
+        // Assessment type.
+        if ($record->assessmenttype == \workshop::PEER_ASSESSMENT) {
+            $assessmenttype = get_string('peerassessment', 'workshop');
+        } else if ($record->assessmenttype == \workshop::SELF_ASSESSMENT) {
+            $assessmenttype = get_string('selfassessment', 'workshop');
+        } else if ($record->assessmenttype == \workshop::SELF_AND_PEER_ASSESSMENT) {
+            $assessmenttype = get_string('selfandpeerassessment', 'workshop');
+        }
+        $mform->addElement('static', 'summary_assessmenttype', get_string('assessmenttype', 'workshop'), $assessmenttype);
+
+        // Grading method.
+        $strategieslist = \workshop::available_strategies_list();
+        $gradingmethod = $strategieslist[$record->strategy];
+        $mform->addElement('static', 'summary_strategy', get_string('strategy', 'workshop'), $gradingmethod);
+
+        // Allow submission.
+        if ($record->allowsubmission == 1) {
+            $mform->addElement('static',
+                'summary_allowsubmission', get_string('allowsubmission', 'workshop'), get_string('yes'));
+        }
+
+        // Submission start.
+        if ($record->submissionstart != 0) {
+            $strdatestring = get_string('strftimerecentfull', 'langconfig');
+            $date = userdate($record->submissionstart, $strdatestring);
+            $mform->addElement('static', 'summary_submissionstart', get_string('submissionstart', 'workshop'), $date);
+        }
+
+        // Submissions deadline.
+        if ($record->submissionend != 0) {
+            $strdatestring = get_string('strftimerecentfull', 'langconfig');
+            $date = userdate($record->submissionend, $strdatestring);
+            $mform->addElement('static', 'summary_submissionend', get_string('submissionend', 'workshop'), $date);
+        }
+
+        // Phase switch assessment.
+        if ($record->submissionend != 0 && $record->phaseswitchassessment != 0) {
+            $mform->addElement('static',
+                'summary_switchassessment', get_string('submissionendswitch', 'workshop'), get_string('yes'));
+        }
+
+        // Allow assessment after submission.
+        if ($record->assessassoonsubmitted != 0) {
+            $mform->addElement('static',
+                'summary_assessassoonsubmitted', get_string('assessassoonsubmitted', 'workshop'), get_string('yes'));
+        }
+
+        // Peer allocation.
+        $userplan = new \workshop_user_plan($this->workshop, $USER->id);
+        if (isset($userplan->phases[\workshop::PHASE_SUBMISSION])
+            && isset($userplan->phases[\workshop::PHASE_SUBMISSION]->tasks)
+            && isset($userplan->phases[\workshop::PHASE_SUBMISSION]->tasks['allocate'])) {
+            $details = $userplan->phases[\workshop::PHASE_SUBMISSION]->tasks['allocate']->details;
+            $mform->addElement('static', 'summary_peerallocationdetails', get_string('allocate', 'workshop'), $details);
+        }
+
+        // Asssess without submission.
+        if ($record->allowsubmission && $record->assesswithoutsubmission) {
+            $mform->addElement('static',
+                'summary_assesswithoutsubmission', get_string('assesswithoutsubmission', 'workshop'), get_string('yes'));
+        }
+
+        // Assessment start.
+        if ($record->assessmentstart != 0) {
+            $strdatestring = get_string('strftimerecentfull', 'langconfig');
+            $date = userdate($record->assessmentstart, $strdatestring);
+            $mform->addElement('static', 'summary_assessmentstart', get_string('assessmentstart', 'workshop'), $date);
+        }
+
+        // Assessment end.
+        if ($record->assessmentend != 0) {
+            $strdatestring = get_string('strftimerecentfull', 'langconfig');
+            $date = userdate($record->assessmentend, $strdatestring);
+            $mform->addElement('static', 'summary_assessmentend', get_string('assessmentend', 'workshop'), $date);
+        }
+
+        $mform->addElement('html', \html_writer::end_div());
+
     }
 
 }

dc6e7e77ffee3e1b459b838a41064fe61aafa469
Paula Ardanza Menendez <paula.ardanza.menendez@umontreal.ca>
2017-05-23 13:36:31 -0400

    MDLUM-5232 - MDL 3.3 - Faire la fusion du code StudiUM avec la nouvelle version
    * Mise a jour du theme_bootstrapbase

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2017032100;
-$plugin->requires  = 2016051900;
+$plugin->version   = 2017051500;
+$plugin->requires  = 2017050500;
 $plugin->component = 'theme_cleanudem';
 $plugin->dependencies = array(
-    'theme_bootstrapbase'  => 2016051900,
+    'theme_bootstrapbase'  => 2017050500,
     'local_custompages'  => 2015021700
 );
